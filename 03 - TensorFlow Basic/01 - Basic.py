7bcce1a4cab79fa8d6420a079888e1cbe4f2191e
dlrudco <37071556+dlrudco@users.noreply.github.com>
2018-09-27 20:42:51 +0900

    minor test

--- 03 - TensorFlow Basic/01 - Basic.py
+++ 03 - TensorFlow Basic/01 - Basic.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 
 # tf.constant: 말 그대로 상수입니다.
 hello = tf.constant('Hello, TensorFlow!')
-print(hello)
+#print(hello)
 
 a = tf.constant(10)
 b = tf.constant(32)
 c = tf.add(a, b)  # a + b 로도 쓸 수 있음
-print(c)
+#print(c)
 
 # 위에서 변수와 수식들을 정의했지만, 실행이 정의한 시점에서 실행되는 것은 아닙니다.
 # 다음처럼 Session 객제와 run 메소드를 사용할 때 계산이 됩니다.
@@ -17,7 +17,7 @@ print(c)
 sess = tf.Session()
 # sess.run: 설정한 텐서 그래프(변수나 수식 등등)를 실행합니다.
 print(sess.run(hello))
-print(sess.run([a, b, c]))
+print(sess.run(a))
 
 # 세션을 닫습니다.
 sess.close()
